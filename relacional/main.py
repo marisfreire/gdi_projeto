a2c20fbb78940ffef48758c8daa768cf379fdd41
Mariana Freire <msf6@cin.ufpe.br>
2025-08-05 16:18:26 -0300

    Ajuste do main.py relacional

--- relacional/main.py
+++ relacional/main.py
@@ -1,8 +1,10 @@
 import sqlite3 as sql3
+import os
 from sqlite3 import OperationalError
 conn = sql3.connect('clinicavet.db')
 print("Bem vindo(a) a Clínica Veterinária:\n")
 print()
+
 
 cursor = conn.cursor()
 
@@ -30,14 +32,22 @@ except OperationalError as e:
     print("Erro ao executar povoamento:", e)
 
 while(continuar):
-    consulta = input(" Digite sua consulta ou digite 0 para sair.")
-    if(consulta == "0"):
+    print(" Digite sua consulta ou digite 0 para sair.\n")
+    linha_consulta = ''
+    consulta_toda =  ''
+    while(linha_consulta != ';' and linha_consulta != '0'):
+        linha_consulta = input()
+        consulta_toda += linha_consulta + ' '
+
+    if(linha_consulta == "0"):
         continuar = False
     else:
-        resultado = cursor.execute(consulta) 
+        resultado = cursor.execute(consulta_toda) 
         linhas = resultado.fetchall()
         for linha in linhas:
-            print(linha) 
-            
+            print(linha)
+
+
+os.remove('./clinicavet.db')
 conn.close()
 print("Até a próxima!\n")
