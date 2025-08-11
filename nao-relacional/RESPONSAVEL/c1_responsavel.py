131020b6b58676af5fc7daa5bded308945169ec6
Byanca <79319402+byancamari@users.noreply.github.com>
2025-08-11 10:41:36 -0300

    Update c1_responsavel.py

--- nao-relacional/RESPONSAVEL/c1_responsavel.py
+++ nao-relacional/RESPONSAVEL/c1_responsavel.py
@@ -9,28 +9,31 @@ def inserir_cenario1(colecao):
     bd.tutor.delete_many({})
     bd.animal.delete_many({})
 
-    # lista de animais
-    animal_doc = [
-        {"_id" : 1, "nome" : "Umbigo"},
-        {"_id" : 4, "nome" : "Paçoca"}, 
-        {"_id" : 6, "nome" : "Bolinha"}, 
-        {"_id" : 7, "nome" : "Luna"}, 
-        {"_id" : 8, "nome" : "Caramelo"}, 
-    ]
-
     # lista de tutores
     tutor_doc = [
-        {"_id": 111, "nome": "Giovanna Mafra", "id_animal": 1}, 
-        {"_id": 222, "nome": "Ana Silva", "id_animal": 6}, 
-        {"_id": 333, "nome": "Paulo Gustavo", "id_animal": 7}, 
-        {"_id": 444, "nome": "Andson Baladeiro, "id_animal": 5}, 
-        {"_id": 555, "nome": "Maria Fernanda", "id_animal": 4},
-        {"_id": 666, "nome": "Pedro Mafra", "id_animal": 1},
-        {"_id": 777, "nome": "Helena Mafra", "id_animal": 1},
+        {"_id": 111, "nome": "Giovanna Mafra"}, 
+        {"_id": 222, "nome": "Ana Silva"}, 
+        {"_id": 333, "nome": "Paulo Gustavo"}, 
+        {"_id": 444, "nome": "Andson Baladeiro"}, 
+        {"_id": 555, "nome": "Maria Fernanda"},
+        {"_id": 666, "nome": "Pedro Mafra"},
+        {"_id": 777, "nome": "Helena Mafra"},
+    ]
+    
+    # lista de animais
+    animal_doc = [
+        {"_id" : 1, "nome" : "Umbigo", "id_tutor": 111},
+        {"_id" : 2, "nome" : "Umbigo", "id_tutor": 666},
+        {"_id" : 3, "nome" : "Umbigo", "id_tutor": 777},
+        {"_id" : 4, "nome" : "Paçoca", "id_tutor": 333}, 
+        {"_id" : 5, "nome" : "Paçoca", "id_tutor": 444}, 
+        {"_id" : 6, "nome" : "Bolinha", "id_tutor": 222}, 
+        {"_id" : 7, "nome" : "Luna", "id_tutor": 444}, 
+        {"_id" : 8, "nome" : "Caramelo", "id_tutor": 555}, 
     ]
 
-    bd.animal.insert_many(animal_doc)
     bd.tutor.insert_many(tutor_doc)
+    bd.animal.insert_many(animal_doc)
     print("Cenário 1 inserido com sucesso!")
 
 def remover_cenario1(colecao):
@@ -39,22 +42,24 @@ def remover_cenario1(colecao):
     print("Cenário 1 deletado com sucesso!")
     print()
 
-def consulta_cenario1():
+def consulta_cenario1(colecao):
     nome_busca = "Umbigo"
 
     # buscar animais com esse nome
-    animais = bd.animal.find({"nome": nome_busca})
-
+   
+   
     # armazenar resultado
-    resultados = []
-
-    for animal in animais:
-        id_animal = tutor.get("id_animal")
-        animal = bd.animal.find({"_id" : id_animal})
-
-        for t in tutor:
-            resultados.append({"id_animal": t['_id'], "nome_tutor": t['nome']})
-
+    resultados = colecao.animal.aggregate([
+        {
+            '$lookup':
+            {
+                'from': 'tutor',
+                'localField': 'id_tutor',
+                'foreignField': '_id',
+                'as': 'tutor'
+            }
+        }
+    ])
     print(f"Os tutores de {nome_busca} são: ")
     for r in resultados:
-        print(f"Tutor ID: {r['tutor_id']} - Tutor: {r['nome_tutor']}")
+        print(f"Tutor ID: {r['_id']} - Tutor: {r['nome']}")
